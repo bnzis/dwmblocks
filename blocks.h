1dfccc28ccc6f619fb327a553f56d2ab96dae52b
bnzis <bnzis.lambda@gmail.com>
2024-05-04 19:02:33 +0200

    added memory script

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
+// clang-format off
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "sbbat",	5,		10},
 	{"", "sbcpu",	30,		2},
 	{"", "sbmem",	30,		1},
+	{"", "sbst",    5,      1},
 	{"", "clock",	5,		3},
 };
 
