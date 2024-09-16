703e4c046b554d4fc182dafda6b43f25480afe26
bnzis <bnzis.lambda@gmail.com>
2024-09-16 12:10:14 +0200

    fixing missing scripts

--- blocks.h
+++ blocks.h
@@ -2,9 +2,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sbbat",	5,		10},
-	{"", "sbcpu",	30,		2},
-	{"", "sbmem",	30,		1},
+	// {"", "sbbat",	5,		10},
+	// {"", "sbcpu",	30,		2},
+	// {"", "sbmem",	30,		1},
 	{"", "sbst",    5,      1},
 	{"", "clock",	5,		3},
 };
